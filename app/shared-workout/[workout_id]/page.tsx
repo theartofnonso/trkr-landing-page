7c274f0322e7b4f4a82cc88129c8fd9f7c3d62dd
Foodate Developer <slimbion@gmail.com>
2024-09-22 16:37:55 +0100

    Fixes

--- app/shared-workout/[workout_id]/page.tsx
+++ app/shared-workout/[workout_id]/page.tsx
@@ -2,13 +2,57 @@
 
 import {Amplify} from 'aws-amplify';
 import {generateClient} from 'aws-amplify/api';
-import config from '@/amplifyconfiguration.json';
 import {getRoutineTemplate} from "@/src/graphql/queries";
 import {useParams} from 'next/navigation'
 import {useEffect, useState} from "react";
 import {BWExerciseSection, DRExerciseSection, WRExerciseSection} from "@/app/components/ExerciseCard";
 import {ExerciseTemplate} from "@/app/components/ExerciseCard";
 import LoadingSpinner from "@/app/components/LoadingSpinner";
+
+const config = {
+    "aws_project_region": "eu-west-2",
+    "aws_mobile_analytics_app_id": "8007617972cc4e2e85f3f412ec30e0f3",
+    "aws_mobile_analytics_app_region": "eu-west-2",
+    "Analytics": {
+        "AWSPinpoint": {
+            "appId": "8007617972cc4e2e85f3f412ec30e0f3",
+            "region": "eu-west-2"
+        }
+    },
+    "aws_cloud_logic_custom": [
+        {
+            "name": "api71b45aa3",
+            "endpoint": "https://6ifisht6d2.execute-api.eu-west-2.amazonaws.com/prod",
+            "region": "eu-west-2"
+        }
+    ],
+    "aws_appsync_graphqlEndpoint": "https://nh2ymicuzzbrdi3xijrwna7vua.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-2",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_cognito_identity_pool_id": "eu-west-2:9e6eabe1-913f-4753-8cf0-f87711f31b12",
+    "aws_cognito_region": "eu-west-2",
+    "aws_user_pools_id": "eu-west-2_iOqG7UL7T",
+    "aws_user_pools_web_client_id": "4p0fhv8552rpfi7k9jc70729lj",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
 
 Amplify.configure(config, {ssr: true});
 
