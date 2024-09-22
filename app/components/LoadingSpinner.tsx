6acce2f9c7855ebcd531a13b638158c464a8c8e8
Foodate Developer <slimbion@gmail.com>
2024-09-22 16:49:50 +0100

    Update LoadingSpinner.tsx

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const LoadingSpinner: React.FC = () => {
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center space-y-4">
+        <div className="fixed inset-0 bg-gradient-to-b from-sapphireDark80 to-sapphireDark flex flex-col items-center justify-center space-y-4">
             <div className="animate-spin rounded-full h-28 w-28 flex items-center justify-center border-t-4 border-white border-solid">
                 <Image
                     className="z-50"
