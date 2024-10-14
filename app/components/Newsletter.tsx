8300db6313aa46f90be0126b1d4d9cdaa1d39dc0
Foodate Developer <slimbion@gmail.com>
2024-10-14 17:25:28 +0100

    Fixes

--- app/components/Newsletter.tsx
+++ app/components/Newsletter.tsx
@@ -1,41 +1,78 @@
+'use client';
 
-import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import {CalendarDaysIcon, HandRaisedIcon} from '@heroicons/react/24/outline'
+import {FormEvent, useState} from "react";
 
 export default function Newsletter() {
+
+    let [email, setEmail] = useState("false")
+    const [isSubscribed, setIsSubscribed] = useState(false);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch('/api/send-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email}),
+            });
+
+            const result = await res.json();
+
+            if (result.success) {
+                setIsSubscribed(true);
+            } else {
+                setIsSubscribed(false);
+            }
+        } catch (error) {
+            setIsSubscribed(false);
+        }
+    };
+
     return (
         <div className="relative isolate overflow-hidden bg-transparent py-16 sm:py-24 lg:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
                     <div className="max-w-xl lg:max-w-lg">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our newsletter.</h2>
+                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our
+                            newsletter.</h2>
                         <p className="mt-4 text-lg leading-8 text-gray-300">
-                            Join a community of people who track their progress, share their wins, and motivate each other to reach new fitness goals,
+                            Join a community of people who track their progress, share their wins, and motivate each
+                            other to reach new fitness goals,
                         </p>
-                        <div className="mt-6 flex max-w-md gap-x-4">
-                            <label htmlFor="email-address" className="sr-only">
-                                Email address
-                            </label>
-                            <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                required
-                                placeholder="Enter your email"
-                                autoComplete="email"
-                                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-vibrantGreen sm:text-sm sm:leading-6"
-                            />
-                            <button
-                                type="submit"
-                                className="flex-none rounded-md bg-vibrantGreen px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                            >
-                                Subscribe
-                            </button>
-                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mt-6 flex max-w-md gap-x-4">
+
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    placeholder="Enter your email"
+                                    autoComplete="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-vibrantGreen sm:text-sm sm:leading-6"
+                                />
+                                <button
+                                    type="submit"
+                                    className="flex-none rounded-md bg-sapphireDark60 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-vibrantGreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-vibrantGreen"
+                                >
+                                    Subscribe
+                                </button>
+
                     </div>
-                    <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        </form>
+                </div>
+                <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
                         <div className="flex flex-col items-start">
                             <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                <CalendarDaysIcon aria-hidden="true" className="h-6 w-6 text-white" />
+                                <CalendarDaysIcon aria-hidden="true" className="h-6 w-6 text-white"/>
                             </div>
                             <dt className="mt-4 font-semibold text-white">New Features</dt>
                             <dd className="mt-2 leading-7 text-gray-400">
@@ -44,7 +81,7 @@ export default function Newsletter() {
                         </div>
                         <div className="flex flex-col items-start">
                             <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                <HandRaisedIcon aria-hidden="true" className="h-6 w-6 text-white" />
+                                <HandRaisedIcon aria-hidden="true" className="h-6 w-6 text-white"/>
                             </div>
                             <dt className="mt-4 font-semibold text-white">Events</dt>
                             <dd className="mt-2 leading-7 text-gray-400">
