dc8516533cd65d069554a1184c1cf8e640f503d4
Foodate Developer <slimbion@gmail.com>
2024-10-14 16:19:57 +0100

    UI changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
   return (
     <header>
       <nav className="">
-        <Container className="relative z-50 flex justify-center pt-8">
+        <Container className="relative z-50 flex justify-center pt-10">
           <div className="relative z-10 flex items-center gap-14">
             <Link href="/" aria-label="Home">
               <TRKRLogo className="h-4 w-auto"/>
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import { Reviews } from '@/app/components/Reviews'
 import { SecondaryFeatures } from '@/app/components/SecondaryFeatures'
 import {Header} from "@/app/components/Header";
 import {Footer} from "@/app/components/Footer";
+import {Hero} from "@/app/components/Hero";
+import Newsletter from "@/app/components/Newsletter";
 
 export default function Home() {
     return (
         <div className="bg-gradient-to-b from-sapphireDark60 to-sapphireDark">
             <Header/>
+            <Hero/>
             <PrimaryFeatures />
             <SecondaryFeatures />
             <Reviews />
             <Faqs />
+            <Newsletter/>
             <Footer/>
         </div>
     )
